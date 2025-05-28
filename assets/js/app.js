b07bb1beed7dc5b02f9fb0ebd4ff89ddbe82c8bb
mathiasotte <128278143+mathiasotte@users.noreply.github.com>
2025-05-28 14:25:15 +0200

    finished modal

---
+++ assets/js/app.js
@@ -0,0 +1,36 @@
+
+// landing page JS ------------------------------------------
+
+// JavaScript for modal funktionalitet
+  const openBtn = document.getElementById('valueBtn');
+  const closeBtn = document.getElementById('closeModalBtn');
+  const modalOverlay = document.getElementById('valueModalContainer');
+  const modal = document.getElementById('valueModal');
+
+  
+
+  // åben modal
+  openBtn.addEventListener('click', () => {
+    modalOverlay.classList.remove('hidden');
+    modal.classList.remove('hidden');
+  });
+
+  // Luk modal når man trykker på "X"
+  closeBtn.addEventListener('click', () => {
+    modalOverlay.classList.add('hidden');
+  });
+
+  // luk modal når man klikker udenfor modal
+  modalOverlay.addEventListener('click', (e) => {
+    if (e.target === modalOverlay) {
+      modalOverlay.classList.add('hidden');
+    }
+    if (e.target === modal) {
+      modal.classList.add('hidden');
+    }
+  });
+
+
+
+
+ 
